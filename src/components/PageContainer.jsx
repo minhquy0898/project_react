bac3d0c6d14d3875061f04b72c078b10bde01266
HoangQuangKy <kyhoang487@gmail.com>
2023-08-18 23:02:48 +0700

    merge code into productPage

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -10,10 +10,10 @@ import ProductDetail from './ProductDetail/ProductDetail'
 function PageContainer() {
     return (
         <div className='PageContainer'>
+            <Header></Header>
             <BrowserRouter>
-                <Header></Header>
-                <Content />
                 <Routes>
+                    <Route path='/' element={<Content></Content>}></Route>
                     <Route path='/product' element={<ProductPageContainer></ProductPageContainer>}></Route>
                     <Route path='/product/:productId' element={<ProductDetail></ProductDetail>}></Route>
                 </Routes>
