f1b9433c88c01aa10e938a26fc022acf75e65ef3
KNT2011 <nguyenngockhanh986@gmail.com>
2023-08-30 13:38:49 +0700

    payment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,7 +6,6 @@ import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import "../node_modules/flag-icon-css/css/flag-icons.min.css"
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
